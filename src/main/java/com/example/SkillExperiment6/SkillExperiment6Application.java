f4d1d73a1c615351b8a279a5c5713707456de9b1
sainagatarak <yarraboinasainagatarak@gmail.com>
2026-02-12 15:15:54 +0530

    first commit

---
+++ src/main/java/com/example/SkillExperiment6/SkillExperiment6Application.java
@@ -0,0 +1,14 @@
+package com.example.SkillExperiment6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class SkillExperiment6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillExperiment6Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/SkillExperiment6/book.java
@@ -0,0 +1,22 @@
+package com.example.SkillExperiment6;
+
+public class book {
+	
+	int bno;
+	String bname;
+	//generate getters and setters 
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public String getBname() {
+		return bname;
+	}
+	public void setBname(String bname) {
+		this.bname = bname;
+	}
+	
+
+}
